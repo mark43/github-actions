da7e183dfd74347b1c7f8a85d4075540c52f9bb9
Johannes Lumpe <johannes@johanneslumpe.de>
2019-11-21 18:00:22 -0500

    clean up `collect-issues`, add tests

--- collect-issues/src/index.ts
+++ collect-issues/src/index.ts
@@ -1,97 +1,94 @@
-import * as core from "@actions/core";
-import { GitHub, context } from "@actions/github";
+import * as core from '@actions/core';
+import { GitHub, context } from '@actions/github';
+import Octokit = require('@octokit/rest');
+import { toArray } from './utils/to-array';
+import { createRegexpFromStrings } from './utils/create-regexp-from-strings';
 
-function getParsedInput<T>(param: string, options?: core.InputOptions): T {
+/**
+ * Gets action input and parses it as json
+ * @param param
+ * @param options
+ */
+const getParsedInput = <T>(
+  param: string,
+  options: core.InputOptions = {
+    required: true,
+  },
+): T => {
   const input = core.getInput(param, options);
-
   try {
     const parsedInput = JSON.parse(input);
     return parsedInput as T;
   } catch (e) {
     throw new Error(`Failed parsing input for ${param}: ${e.message}`);
   }
-}
+};
 
-function toArray<T>(data: T | T[]): T[] {
-  if (Array.isArray(data)) {
-    return data;
-  }
+/**
+ * Filters issues by title and label filters. When an array of filters is passed
+ * it is assumed that _some_ filter in the array has to match, not all
+ */
+const filterIssuesByLabelAndTitle = ({
+  labelFilters,
+  titleFilters,
+  issues,
+}: {
+  labelFilters: string[];
+  titleFilters: string[];
+  issues: Octokit.SearchIssuesAndPullRequestsResponseItemsItem[];
+}): Octokit.SearchIssuesAndPullRequestsResponseItemsItem[] => {
+  const labelRegexp = labelFilters.length
+    ? createRegexpFromStrings(labelFilters)
+    : undefined;
+  const titleRegexp = titleFilters.length
+    ? createRegexpFromStrings(titleFilters)
+    : undefined;
 
-  return [data];
-}
-
-function cartesianProduct<T>(a: T[], b: T[]): T[][] {
-  if (!a.length && !b.length) {
-    return [];
-  }
-
-  const outer: T[] = a.length ? a : b;
-  const inner: T[] = a.length ? b : a;
-
-  return outer.flatMap(item => {
-    return inner.length ? inner.map(innerItem => [item, innerItem]) : [[item]];
-  });
-}
-
-function getQueryTuples(
-  titleFilters: string | string[],
-  labelFilters: string | string[]
-): string[][] {
-  const titleFilterArray = toArray(titleFilters).map(
-    title => `${title} in:title`
+  return issues.filter(
+    item =>
+      (!labelRegexp ||
+        item.labels.some(labelItem => labelRegexp.test(labelItem.name))) &&
+      (!titleRegexp || titleRegexp.test(item.title)),
   );
-  const labelFilterArray = toArray(labelFilters).map(label => `label:${label}`);
-  return cartesianProduct(titleFilterArray, labelFilterArray);
-}
+};
 
-async function run() {
+export const run = async (): Promise<void> => {
   try {
-    const resultEnvVariable = getParsedInput<string>("env-output-variable", {
-      required: true
-    });
-    const titleFilters = getParsedInput<string | string[]>("titlefilters", {
-      required: true
-    });
-    const labelFilters = getParsedInput<string | string[]>("labelFilters", {
-      required: true
-    });
-    const repoToken = getParsedInput<string>("repo-token", { required: true });
-
-    const searchQueries = getQueryTuples(titleFilters, labelFilters);
+    const resultEnvVariable = getParsedInput<string>('env-output-variable');
+    const titleFilters = toArray(
+      getParsedInput<string | string[]>('titlefilters'),
+    );
+    const labelFilters = toArray(
+      getParsedInput<string | string[]>('labelFilters'),
+    );
+    const repoToken = getParsedInput<string>('repo-token');
     const github = new GitHub(repoToken);
 
-    // NOTE: the github api does not support querying by multiple labels at the same time and treating the condition
-    // as a boolean OR. Because of this we have to generate the cartesian product between title filters and label filters
-    // and execute a search for each of them. This is not ideal as the number of requests can quickly grow
-    // We might want to execute these request sequentially instead of in parallel to prevent hammering the api.
-    // Currently we do not support paging through search results, as we expect each query to return less than 100 results
-    // Note: Github actions have a limit of 1000 api requests per hour across all actions.
-    const allIssues = await Promise.all(
-      searchQueries.map(queryTuple => {
-        console.log(`Pulling issues for query: ${queryTuple.join(" ")}...`);
-        return github.search.issuesAndPullRequests({
-          q: `in:${context.repo.owner}/${
-            context.repo.repo
-          } is:open is:issue ${queryTuple.join(" ")}`
-        });
-      })
-    );
+    const query = `repo:${context.repo.owner}/${context.repo.repo} is:open is:issue`;
+    console.log('Executing query:', query);
+    const allIssues = await github.search.issuesAndPullRequests({
+      q: query,
+    });
 
-    const issueIds = allIssues.flatMap(issues =>
-      issues.data.items.map(issue => issue.id)
-    );
-
+    const filteredIssues = filterIssuesByLabelAndTitle({
+      labelFilters,
+      titleFilters,
+      issues: allIssues.data.items,
+    });
+    const issueIds = filteredIssues.map(issue => issue.id);
     console.log(`Found ${issueIds.length} issues:`, issueIds);
 
     // Expose serialized result both as output and as environment variable so other actions can
     // consume it however they see fit.
-    console.log("Exporting results...");
+    console.log('Exporting results...');
     const serializedOutput = JSON.stringify(issueIds);
     core.exportVariable(resultEnvVariable, serializedOutput);
-    core.setOutput("issueids", serializedOutput);
+    core.setOutput('issueids', serializedOutput);
   } catch (error) {
     core.setFailed(error.message);
   }
-}
+};
 
-run();
+if (process.env.NODE_ENV !== 'test') {
+  run();
+}
